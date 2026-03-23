960597d169ef51ec74748437d3655b6380802540
Sawelch2027 <Sawelch@email.sc.edu>
2026-03-22 23:39:23 -0400

    live

--- divefitness-react/src/App.js
+++ divefitness-react/src/App.js
@@ -9,7 +9,7 @@ import About from "./pages/about/about";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
 
       <Routes>
